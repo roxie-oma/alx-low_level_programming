19d5c83bee99090013833490feb2d915bfd86ae6
roxie-oma <chikwadorose20@gmail.com>
2022-03-15 00:45:44 +0000

    My c code task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: prints the size of various types on the computer it is 
+* compiled and run on
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+char c;
+long int l;
+long long int ll;
+float f;
+
+printf("the size of an int is: %lu .\n", (unsigned long) sizeof(i));
+printf("the size of an  char is: %lu .\n", (unsigned long) sizeof(c));
+printf("the size of an long int is: %lu .\n", (unsigned long) sizeof(l));
+printf("the size of an long long int is: %lu .\n", (unsigned long) sizeof(ll));
+printf("the size of an float is: %lu .\n", (unsigned long) sizeof(f));
+
+return (0);
+
+}
+
