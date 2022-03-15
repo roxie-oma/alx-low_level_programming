c341197fb30ecfc66437b3cec1eb807206d8130f
roxie-oma <chikwadorose20@gmail.com>
2022-03-15 03:12:11 +0000

    last task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+* main - print quote
+*
+* Description: prints Dora's quote
+*
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+char *msg = "and that piece of art is useful\"- \"Doraa Korpar, 2015-1-19\n";
+system("echo and that piece of art is useful\"- \"Dora Korpar, 215-10-19\n");
+return (1);
+}
