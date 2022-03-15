93abc6bdf10dd54352cab078916c67b0b65dcd30
roxie-oma <chikwadorose20@gmail.com>
2022-03-15 00:53:32 +0000

    last task on c basic

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,13 +15,12 @@ long int l;
 long long int ll;
 float f;
 
-printf("the size of an int is: %lu .\n", (unsigned long) sizeof(i));
-printf("the size of an  char is: %lu .\n", (unsigned long) sizeof(c));
-printf("the size of an long int is: %lu .\n", (unsigned long) sizeof(l));
-printf("the size of an long long int is: %lu .\n", (unsigned long) sizeof(ll));
-printf("the size of an float is: %lu .\n", (unsigned long) sizeof(f));
+printf("the size of an int is: %lu byte(s)\n", (unsigned long) sizeof(i));
+printf("the size of an  char is: %lu byte(s)\n", (unsigned long) sizeof(c));
+printf("the size of an long int is: %lu byte(s)\n", (unsigned long) sizeof(l));
+printf("the size of an long long int is: %lu byte(s)\n", (unsigned long) sizeof(ll));
+printf("the size of an float is: %lu byte(s)\n", (unsigned long) sizeof(f));
 
 return (0);
-
 }
 
